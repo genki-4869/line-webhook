fc5f2487d919b64784595f9d5cbc00e2ae52a7d3
genki-4869 <genki.godblue@iCloud.com>
2025-09-21 23:29:02 +0900

    初回コミット

---
+++ webhook.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+import requests, json
+
+app = Flask(__name__)
+
+ACCESS_TOKEN = '1eWjiMP/MusTUmfEnR9mo48kJNHHJLmz+C0c8v+74ogqym1YGRryOLQWcASizMORchMZLqw1PnunoZr8CnfDzgLeF2wUF46o3Cx7wFKt6GXftfYzDwbcxlh9RXYvZr9sfHOI2EzCUzHcw9BiSQf39wdB04t89/1O/w1cDnyilFU='
+REPLY_API = 'https://api.line.me/v2/bot/message/reply'
+
+@app.route("/webhook", methods=['POST'])
+def webhook():
+    body = request.json
+    for event in body['events']:
+        if event['type'] == 'message' and event['message']['type'] == 'text':
+            reply_token = event['replyToken']
+            user_text = event['message']['text']
+
+            reply_data = {
+                "replyToken": reply_token,
+                "messages": [
+                    {
+                        "type": "text",
+                        "text": f"「{user_text}」って言ったね！"
+                    }
+                ]
+            }
+
+            headers = {
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {ACCESS_TOKEN}"
+            }
+            requests.post(REPLY_API, headers=headers, data=json.dumps(reply_data))
+    return "OK"
